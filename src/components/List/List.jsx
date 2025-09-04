fd6df7a80942351733c2a1557d6d2c7bc51a6acc
Катерина Чакмазова <chakmazova06@gmail.com>
2025-09-04 21:35:47 +0300

    Add store to List and ListItem

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,22 @@
 import ListItem from '../ListItem/ListItem';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getContacts } from '../../store/actions/contactsActions';
+import api from './api/contacts-service.js';
 import './List.css';
-function List({ contacts = [], onDelete, transferContact }) {
+function List({ transferContact }) {
+  const dispatch = useDispatch();
+  async function getContactsFromStorage() {
+    try {
+      const response = await api.get('/');
+      dispatch(getContacts(response.data));
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  useEffect(() => {
+    getContactsFromStorage();
+  }, [getContacts]);
   return (
     <div className='list-container'>
       <h2>Contact List</h2>
@@ -8,7 +24,6 @@ function List({ contacts = [], onDelete, transferContact }) {
         <ListItem
           key={contact.id}
           contact={contact}
-          onDelete={onDelete}
           transferContact={transferContact}
         />
       ))}
--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,5 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../store/actions/contactsActions';
+import api from './api/contacts-service.js';
 import './ListItem.css';
-function ListItem({ contact, transferContact, onDelete }) {
+function ListItem({ contact, transferContact }) {
+  const dispatch = useDispatch();
+  async function onDelete(id) {
+    try {
+      await api.delete(`/${id}`);
+      dispatch(deleteContact(id));
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
   const { fName, lName, id } = contact;
   return (
     <div className='list-item'>
